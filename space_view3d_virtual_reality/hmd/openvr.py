9b7050693267c1535f2130cc4a9d472096fef315
William Culver <bill@cedeon.co.uk>
2016-05-03 15:53:53 +0100

    Added init

--- space_view3d_virtual_reality/hmd/openvr.py
+++ space_view3d_virtual_reality/hmd/openvr.py
@@ -8,8 +8,6 @@ It uses a python wrapper to connect with the SDK
 
 from . import HMD_Base
 
-from .oculus import Oculus
-
 from ..lib import (
         checkModule,
         )
@@ -22,3 +20,40 @@ class OpenVR(HMD_Base):
     def _getHMDClass(self):
         from bridge.hmd.openvr import HMD
         return HMD
+
+    def init(self, context):
+        """
+        Initialize device
+
+        :return: return True if the device was properly initialized
+        :rtype: bool
+        """
+        try:
+            HMD = self._getHMDClass()
+            self._hmd = HMD()
+
+            # gather arguments from HMD
+
+            self.setEye(0)
+            self.width = self._hmd.width_left
+            self.height = self._hmd.height_left
+
+            self.setEye(1)
+            self.width = self._hmd.width_right
+            self.height = self._hmd.height_right
+
+            # initialize FBO
+            if not super(OpenVR, self).init():
+                raise Exception("Failed to initialize HMD")
+
+            # send it back to HMD
+            if not self._setup():
+                raise Exception("Failed to setup OpenVR Compatible HMD")
+
+        except Exception as E:
+            self.error("init", E, True)
+            self._hmd = None
+            return False
+
+        else:
+            return True
